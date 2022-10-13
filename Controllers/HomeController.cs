a2b2df0ed7af235885180a6ce3be2ae6725073a3
VALENTIN JULIAN LASORSA NIKCEVICH <46919212@ort>
2022-10-13 15:35:20 -0300

    Falta mostrar lo del json

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,4 +28,8 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+    public IActionResult CargarRestaurantes()
+    {
+        return View("Views/Home/Restaurantes.cshtml");
+    }
 }
