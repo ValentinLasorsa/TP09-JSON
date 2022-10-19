be08172fa4eaa9cc25e5ea8ee3b749bf70a14a45
VALENTIN JULIAN LASORSA NIKCEVICH <46919212@ort>
2022-10-19 09:02:05 -0300

    falta mostrar JOSN

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,8 +28,5 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
-    public IActionResult CargarRestaurantes()
-    {
-        return View("Views/Home/Restaurantes.cshtml");
-    }
+   
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,10 @@
 {
     console.log(data);
 });
+
+function CargarDatos()
+{ $.getJSON('/TP9JSON.json', 
+function(data) 
+{ console.log(data); 
+    data.Restaurantes.forEach(unrestaunte => { $("#bodyTabla").append("<tr>"); $("#bodyTabla").append("<td><img src='" + unrestaunte.Logo + "'></td>"); $("#bodyTabla").append("<td>" + unrestaunte.Id + "</td>"); $("#bodyTabla").append("<td>" + unrestaunte.Nombre + "</td>"); $("#bodyTabla").append("<tr>")})}
+)}
